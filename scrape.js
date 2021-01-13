239266dcc2efdcdc1f34b0027c2f279db4e418cc
sujanpoojary <sujanpoojary884@gmail.com>
2021-01-13 19:16:10 +0530

    additional method

--- scrape.js
+++ scrape.js
@@ -5,8 +5,19 @@ axios
   .get("https://jamesclear.com/best-articles-2016")
   .then((response) => {
     const $ = cheerio.load(response.data);
-    const pageHeader = $(".footer");
-    console.log(pageHeader.find(".footer__logo").html());
+    // const pageHeader = $(".footer");
+    /* simple jquery stuff */
+    // console.log(pageHeader.find(".footer__logo").html());
+    // console.log(pageHeader.children("div.container").text());
+    // console.log(pageHeader.children("div.container").parent().text());
+
+    /* Looping  */
+    $(".main .header .container div").each((i, el) => {
+      //   console.log("class name");
+      const text = $(el).text();
+      const className = $(el).attr("class");
+      console.log(className);
+    });
   })
   .catch((err) => {
     console.log(err);
